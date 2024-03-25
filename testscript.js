1a77ec130de1aaf3b034298aba8019599fae0f15
djhenry1 <dhenry1@guilford.edu>
2024-03-25 10:56:57 -0400

    website

--- testscript.js
+++ testscript.js
@@ -3,3 +3,7 @@ myitem = document.getElementById("firsttest");
 myitem.addEventListener("click", onClick);
 
 function onClick() {myitem.style.color = "red";}
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {otheritem.style.color = "red";}
